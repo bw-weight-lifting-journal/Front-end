32ff9627d3233a487cdd3b06d99472bcb976561f
Dan <dan.deadward@gmail.com>
2019-07-29 18:18:22 -0400

    testimonial section done

--- weightlifting-journal/src/App.js
+++ weightlifting-journal/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.scss';
 import TopNav from './components/navmenus/Nav';
 import Footer from './components/navmenus/Footer';
+import BottomCTA from './components/contentcontainers/BottomCTA';
+import Testimonials from './components/contentcontainers/Testimonials';
+
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <Testimonials />
+      <BottomCTA />
       <Footer />
     </div>
   );
