e8e31bd7b76c4f370bb05445dc0a6d19a2db341a
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-01 19:22:13 -0600

    Revert "reverting issues"
    This reverts commit ebf0c80e9688359be91d453ed4391fbd856fa145.

--- weightlifting-journal/src/components/contentcontainers/MainbuttonsContainer.js
+++ weightlifting-journal/src/components/contentcontainers/MainbuttonsContainer.js
@@ -5,33 +5,16 @@ import { Link as WorkoutLink } from "react-router-dom";
 import "./MainButtonContainer.scss";
 
 function MainButtonContainer() {
-
-  let tkn = localStorage.getItem('token');
-
-  const deleteTkn = event => {
-    localStorage.removeItem('token');
-  }
-  
   return (
     <Jumbotron fluid>
     <div className="mainbtncontainer">
-    { tkn ?
-      <WorkoutLink className="mainBtn" to="/dashboard">
-      <MainButton 
-        title = "Track Your Workout"
-        content = "Track your workouts by weight, sets, reps, exercise, and much more"
-        arrow = "→"
-      />
-      </WorkoutLink>
-      :
-      <WorkoutLink className="mainBtn" to="/login">
+    <WorkoutLink className="mainBtn" to="/login">
     <MainButton 
       title = "Track Your Workout"
       content = "Track your workouts by weight, sets, reps, exercise, and much more"
       arrow = "→"
     />
-        </WorkoutLink>
-      }
+    </WorkoutLink>
     <div className="mainBtn">
     <MainButton 
       title = "Crush Your Workout"
